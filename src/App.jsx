7639c07a96254b49ded458fcfc01f19ff7542c26
ThomasLeao <thomasleao03@gmail.com>
2024-03-02 21:19:41 -0300

    Criando tela inicial com react e modificando o style.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="TelaInicial">
+      <header className="App-header">
+        <div className='areas'>
+          <a href=""><p>INFANTIL</p></a>
+        </div>
+
+        <div className='areas'>
+          <a href=""><p>ENSINO MÉDIO</p></a>
+        </div>
+
+        <div className='areas'>
+          <a href=""><p>SUPERIOR</p></a>
+        </div>
+
+        <div className='areas'>
+          <a href=""><p>EJA</p></a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
